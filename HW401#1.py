f2b608a4187cc4e8025e0d955a411d02b775b137
AhmadKhan-ops <ahk.khan.kn@gmail.com>
2026-01-29 21:28:11 +0000

    changes

--- HW401#1.py
+++ HW401#1.py
@@ -1,1 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+
+class archINVIN(ABC):
+      def __init__(self, name, price, availability )
+                self.name = name
+                self.availability = availability
+                self.price = price 
+      def get_name(self)
+        return self.name
+      def get_price(self)
+        return self.price 
+      def get_name(self)
+        return self.availability
+
+    @abstractmethod
+    def over(self):
+        pass 
+
 
