2ed5d4fe96b41286cc3a4cbe6e5e09bc41627db5
AhmadKhan-ops <ahk.khan.kn@gmail.com>
2026-01-30 13:48:32 -0600

    Update HW401#1.py

--- HW401#1.py
+++ HW401#1.py
@@ -1,21 +1,30 @@
 from abc import ABC, abstractmethod
-
-
+from typing import Set
 
 class archINVIN(ABC):
-      def __init__(self, name, price, availability )
+      def __init__(self, name, price, availability):
                 self.name = name
                 self.availability = availability
                 self.price = price 
-      def get_name(self)
+      def get_name(self):
         return self.name
-      def get_price(self)
+      def get_price(self):
         return self.price 
-      def get_name(self)
+      def get_name(self):
         return self.availability
 
-    @abstractmethod
-    def over(self):
+      @abstractmethod
+      def over(self):
         pass 
 
+class ItemCate:
+    def __init__(self,name):
+        self.name = name
+        self.itemlist: Set[archINVIN] = set()
+        self.subcate: Set["Category"] = set()
+
+    def get_name(self):
+        return self.name
+    def get_itemlist(self):
+        return self.itemlist
 
